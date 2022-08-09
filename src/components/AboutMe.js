d5a3316ba2fc3f04972be3070b292c54be7701af
unknown <epeng1994@gmail.com>
2022-08-08 23:51:08 -0400

    implement About Me section

---
+++ src/components/AboutMe.js
@@ -0,0 +1,18 @@
+
+
+function AboutMe(){
+    return(
+        <div className = 'aboutme'>
+            <h1>About Me</h1>
+            <div className = 'aboutmeinfo'>
+                <img src = './assets/profilepic.jpg' alt = 'profile pic'/>
+                <p>Welcome! I'm Eric, a Full Stack Web Developer who recently graduated from the Bloomtech Bootcamp Web Development program. Prior to becoming a full stack developer, I had experience within healthcare insurance and book keeping. Fun fact: I enjoy trying new cooking recipes. Contact me below if you'd like to know more!</p>      
+            </div>
+        </div>
+    )
+}
+
+
+
+
+export default AboutMe
---
+++ src/components/Banner.js
@@ -0,0 +1,20 @@
+import {Link} from 'react-router-dom'
+
+function banner(){
+
+    return(
+        <div className ='banner-div'>
+            <h1>Eric Peng's Portfolio</h1>
+            <nav>
+                <h4>Home</h4>
+                <h4>About Me</h4>
+                <h4>Skills</h4>
+                <h4>Projects</h4>
+                <h4>Contact Me</h4>
+            </nav>
+        </div>
+    )
+}
+
+
+export default banner
