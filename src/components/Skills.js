04ef1994bcf3d91657d99cb8c9e5e0fa606e4572
unknown <epeng1994@gmail.com>
2022-08-11 21:52:46 -0400

    initialized skills component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,9 +3,24 @@
 function Skills(){
     return(
         <div className = 'skills'>
-            <h2>Skills</h2>
+            <h1>Skills</h1>
+            <div className ='skillsicons'>
+                <img src = './assets/CSS.png' alt='css icon'/>
+                <img src = './assets/HTML5.png' alt='html5 icon'/>
+                <img src = './assets/JS.png' alt='js icon'/>
+                <img src = './assets/NODEJS.png' alt='nodejs icon'/>
+                <img src = './assets/REACT.png' alt='react icon'/>
+                <img src = './assets/REDUX.png' alt='redux icon'/>
+                <img src = './assets/GIT.png' alt='gur icon'/>
+                <img src = './assets/API.png' alt='API icon'/>
+                <img src = './assets/GITHUB.png' alt='GITHUB icon'/>
+                <img src = './assets/JEST.png' alt='JEST icon'/>
+                <img src='./assets/VS1.png' alt='VS icon' />
+            </div>
         </div>
     )
 }
+
+//<a href="https://www.flaticon.com/free-icons/sql" title="sql icons">Sql icons created by juicy_fish - Flaticon</a>
 
 export default Skills
