0a2e2875db021fdc45ed30ed4af599ba89acc762
unknown <epeng1994@gmail.com>
2022-09-06 17:40:02 -0400

    downloaded emailjs library, initiated simple email form

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,28 +1,38 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 
+const serviceID = process.env.REACT_APP_MY_SERVICE_ID
+const templateID = process.env.REACT_APP_MY_TEMPLATE_ID
+const userID = process.env.REACT_APP_MY_USER_ID
 
+const ContactMe = () => {
 
-function ContactMe(){
+  const form = useRef();
+  const sendEmail = e => {
+    e.preventDefault();
+    emailjs.sendForm(serviceID, templateID, e.target, userID)
+      .then((result) => {
+          console.log(result);
+      }, (error) => {
+          console.log(error);
+      });
+  };
 
-    const handleEmailSubmit = e =>{
-        e.preventDefault();
-    }
+  return (
+    <div className = 'contactMeContainer'>
+        <form ref={form} onSubmit={sendEmail}className ='contactMeForm'>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+        </form>
+    </div>
 
-
-    return(
-        <div className = 'contactMeContainer'>
-            <form className ='contactMeForm'>
-                <h2>Contact me section</h2>
-                <label for='nameInput'>Enter your name.</label>
-                <input type='text' placeholder='Enter name'/>
-                <label for='emailInput'>Enter your email.</label>
-                <input type='text' placeholder='Enter email'/>
-                <label for='messageInput'>Enter your message.</label>
-                <input type='text' placeholder='Enter message'/>
-                <button onSubmit = {()=>handleEmailSubmit}>Send!</button>
-            </form>
-        </div>
-    )
-}
+  );
+};
  
 export default ContactMe;
