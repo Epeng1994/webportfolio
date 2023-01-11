54e0c0bf27234acc124f59958cf62279f37f1837
unknown <epeng1994@gmail.com>
2023-01-11 18:13:07 -0500

    changed css for App, updated path for new components within folder

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import Banner from './components/Banner';
 import AboutMe from './components/AboutMe';
 import Skills from './components/Skills';
-import Projects from './components/Projects';
-import ContactMe from './components/ContactMe';
+import Projects from './components/ProjectComponent/Projects';
+import ContactMe from './components/ContactMeComponent/ContactMe';
 
 function App() {
   return (
